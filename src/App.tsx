0c5a0950abcce24dbfc1c862d64a78ca513f79c6
Gustavo <kg.2k.pl@gmail.com>
2024-07-10 10:29:59 -0400

    creating a listing page and importing react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Listado from './pages/listado';
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <div>
+          <Routes>
+            <Route path='/' element={<Listado />}/>
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/listado.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Listado = () => {
+    return (
+        <>
+            <h1>Pokemon Fav</h1>
+        </>
+    )
+}
+
+export default Listado;
