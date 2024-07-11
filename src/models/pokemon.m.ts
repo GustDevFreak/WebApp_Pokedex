3e6dfe624cc99414b290344b3aef0191dbd60bd9
Gustavo <kg.2k.pl@gmail.com>
2024-07-11 10:02:28 -0400

    creating the model

---
+++ src/models/pokemon.m.ts
@@ -0,0 +1,14 @@
+export type Pokemon = {
+    id: string;
+    name: string;
+    img_gif: string;
+    img_large: string;
+    img_normal: string;
+    total: string;
+    hp: string;
+    attack: string;
+    defense: string;
+    sp_atk: string;
+    sp_def: string;
+    speed: string;
+}
